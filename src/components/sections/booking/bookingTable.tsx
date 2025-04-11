80ee52ab266cdc7ee949c9d0e970adf8bd210629
adelaw99 <adelegbelawrence@gmail.com>
2025-04-11 16:46:17 +0100

    setup dashboard layout and components

---
+++ src/components/sections/booking/bookingTable.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material"
+// import { DataGrid } from "@mui/x-data-grid"
+
+const BookingTable=()=>{
+    return(
+        <Box sx={{
+            height:350,
+            mt:1.75,
+        }}>
+            {/* <DataGrid/> */}
+        </Box>
+    )
+}
+
+export default BookingTable
---
+++ src/routes/router.tsx
@@ -0,0 +1,68 @@
+import { Outlet, createBrowserRouter } from 'react-router-dom';
+import paths, { rootPaths } from './paths';
+import { Suspense, lazy } from 'react';
+import Progress from 'components/loading/Progress';
+import LinearLoader from 'components/loading/LinearLoader';
+
+const App = lazy(() => import('App'));
+const MainLayout = lazy(() => import('layouts/main-layout'));
+const AuthLayout = lazy(() => import('layouts/auth-layout'));
+const Dashboard = lazy(() => import('pages/dashboard/Dashboard'));
+const Bookings = lazy(()=> import('pages/booking/Booking'))
+const Login = lazy(() => import('pages/authentication/Login'));
+const Signup = lazy(() => import('pages/authentication/Signup'));
+const ErrorPage = lazy(() => import('pages/errors/ErrorPage'));
+
+const routes = [
+  {
+    element: (
+      <Suspense fallback={<Progress />}>
+        <App />
+      </Suspense>
+    ),
+    children: [
+      {
+        path: rootPaths.root,
+        element: (
+          <MainLayout>
+            <Suspense fallback={<LinearLoader />}>
+              <Outlet />
+            </Suspense>
+          </MainLayout>
+        ),
+        children: [
+          {
+            index: true,
+            element: <Dashboard />,
+          },
+          {
+            path: "bookings",
+            element: <Bookings />,
+          },
+        ],
+      },
+      {
+        path: rootPaths.authRoot,
+        element: <AuthLayout />,
+        children: [
+          {
+            path: paths.login,
+            element: <Login />,
+          },
+          {
+            path: paths.signup,
+            element: <Signup />,
+          },
+        ],
+      },
+      {
+        path: '*',
+        element: <ErrorPage />,
+      },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+export default router;
